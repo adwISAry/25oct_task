34073c5792692438b5e633fa5d4d408a9e624bd3
birinci novbe <birinci novbe@DESKTOP-R4EGFJU>
2023-10-25 11:03:15 +0400

    Add project files.

---
+++ 25oct_task/Program.cs
@@ -0,0 +1,160 @@
+﻿namespace _25oct_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon myWeapon = null;
+
+            while (true)
+            {
+                Console.WriteLine("Enter your choice: \n<<<0 for gun information" +
+                    ">>>\n<<<1 for Shoot" +
+                    ">>>\n<<<2 for Fire All" +
+                    ">>>\n<<<3 for Get Needed Bullet Count" +
+                    ">>>\n<<<4 for Reload" +
+                    ">>>\n<<<5 for Change Fire Mode" +
+                    ">>>\n<<<6 for Exit" +
+                    ">>>\n<<<w to create a weapon>>>:");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "0":
+                        if (myWeapon != null)
+                            DisplayGunInfo(myWeapon);
+                        else
+                            Console.WriteLine("No weapon created yet.");
+                        break;
+                    case "1":
+                        if (myWeapon != null)
+                            myWeapon.Shoot();
+                        else
+                            Console.WriteLine("No weapon created yet.");
+                        break;
+                    case "2":
+                        if (myWeapon != null)
+                            myWeapon.Fire();
+                        else
+                            Console.WriteLine("No weapon created yet.");
+                        break;
+                    case "3":
+                        if (myWeapon != null)
+                        {
+                            int neededBullets = myWeapon.GetNeededBulletCount();
+                            Console.WriteLine("Needed bullets: " + neededBullets);
+                        }
+                        else
+                            Console.WriteLine("No weapon created yet.");
+                        break;
+                    case "4":
+                        if (myWeapon != null)
+                            myWeapon.Reload();
+                        else
+                            Console.WriteLine("No weapon created yet.");
+                        break;
+                    case "5":
+                        if (myWeapon != null)
+                            myWeapon.ChangeFireMode();
+                        else
+                            Console.WriteLine("No weapon created yet.");
+                        break;
+                    case "6":
+                        Environment.Exit(0);
+                        break;
+                    case "w":
+                        myWeapon = CreateWeapon();
+                        Console.WriteLine("New weapon created.");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static Weapon CreateWeapon()
+        {
+            Weapon newWeapon = new Weapon();
+
+            bool validInput = false;
+
+            do
+            {
+                Console.Write("Enter bullet capacity: ");
+                newWeapon.bulletCap = Convert.ToInt32(Console.ReadLine());
+                if (newWeapon.bulletCap <= 0)
+                {
+                    Console.WriteLine("Not possible.");
+                }
+                else
+                {
+                    validInput = true;
+                }
+            } while (!validInput);
+
+            validInput = false;
+
+            do
+            {
+                Console.Write("Enter current bullets: ");
+                newWeapon.currentBullets = Convert.ToInt32(Console.ReadLine());
+                if (newWeapon.currentBullets < 0 || newWeapon.currentBullets > newWeapon.bulletCap)
+                {
+                    Console.WriteLine("Not possible.");
+                }
+                else
+                {
+                    validInput = true;
+                }
+            } while (!validInput);
+
+            validInput = false;
+
+            do
+            {
+                Console.Write("Enter no bullet time: ");
+                newWeapon.nobulletTime = Convert.ToInt32(Console.ReadLine());
+                if (newWeapon.nobulletTime <= 0)
+                {
+                    Console.WriteLine("Not possible.");
+                }
+                else
+                {
+                    validInput = true;
+                }
+            } while (!validInput);
+
+            validInput = false;
+
+            do
+            {
+                Console.Write("Enter shooting mode (1 for single, 2 for auto): ");
+                newWeapon.shootingMode = Convert.ToInt32(Console.ReadLine());
+                if (newWeapon.shootingMode != 1 && newWeapon.shootingMode != 2)
+                {
+                    Console.WriteLine("Wrong Input. Please enter 1 for single or 2 for auto.");
+                }
+                else
+                {
+                    validInput = true;
+                }
+            } while (!validInput);
+
+            return newWeapon;
+        }
+
+        static void DisplayGunInfo(Weapon weapon)
+        {
+            Console.WriteLine("Bullet Capacity: " + weapon.bulletCap);
+            Console.WriteLine("Current Bullets: " + weapon.currentBullets);
+            Console.WriteLine("No Bullet Time: " + weapon.nobulletTime);
+            Console.WriteLine("Shooting Mode: " + weapon.shootingMode);
+        }
+
+    }
+}
+
+
+      
---
+++ 25oct_task/Weapon.cs
@@ -0,0 +1,82 @@
+﻿namespace _25oct_task
+{
+    internal class Weapon
+    {
+
+        public int bulletCap;
+        public int currentBullets;
+        public int nobulletTime;
+        public int shootingMode;
+
+
+        public void Shoot()
+        {
+            Console.WriteLine("Weapon fired once.");
+            currentBullets -= 1;
+        }
+        public void Fire()
+        {
+            int OneBulletTime = bulletCap / nobulletTime;
+            if (shootingMode == 1)
+            {
+                Console.WriteLine("Weapon fired once. Time taken: " + OneBulletTime + " milliseconds");
+                currentBullets -= 1;
+            }
+            else if (shootingMode == 2)
+            {
+                Console.WriteLine("All bullets fired. Time taken: " + OneBulletTime * currentBullets + " milliseconds");
+                currentBullets = 0;
+            }
+            else
+            {
+                Console.WriteLine("Wrong fire mode. Please select single-fire or automatic.");
+            }
+        }
+
+        public int GetNeededBulletCount()
+        {
+            return bulletCap - currentBullets;
+        }
+        public void Reload()
+        {
+            Console.WriteLine("Weapon reloaded.");
+            currentBullets = bulletCap;
+        }
+
+        public void ChangeFireMode()
+        {
+            Console.WriteLine("Enter 1 for single fire, 2 for automatic:");
+            shootingMode = Convert.ToInt32(Console.ReadLine());
+            if (shootingMode == 1)
+            {
+                Console.WriteLine("Weapon fired once.");
+                currentBullets--;
+            }
+            else
+            {
+                if (shootingMode == 2)
+                {
+                    Console.WriteLine("Your Firing mode is now auto");
+                }
+                else
+                {
+                    if (shootingMode >= 3 || shootingMode < 0)
+                    {
+                        Console.WriteLine("Wrong input");
+                    }
+                }
+            }
+        
+        }
+        public void Exit()
+        {
+            Console.WriteLine("Click 6 to exit:");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if (input == 6)
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
